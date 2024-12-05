a85bbaa22e2e60df81414856d1a5600beaf79891
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-12-05 11:19:15 +0530

    custom hash & Eq impleemntations

--- play-with-rust/src/main.rs
+++ play-with-rust/src/main.rs
@@ -3,9 +3,8 @@ mod map_example2;
 
 
 fn main(){
-
     //map_example1::find_total_price_of_each_dep();
-    // map_example2::get_car_details();
+    map_example2::get_car_details();
 
 }
 
--- play-with-rust/src/map_example2.rs
+++ play-with-rust/src/map_example2.rs
@@ -3,11 +3,30 @@
 // input key ( i.e Owner ) and get value ( i.e Car )
 
 
-#[derive(Eq, PartialEq,Hash)]
 struct Owner {
     name: String,
     age: u32,
 }
+
+// implement Hash for Owner only based on name
+impl std::hash::Hash for Owner {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+    }
+}
+
+// implement Eq for Owner only based on name
+impl std::cmp::Eq for Owner {}
+
+// implement PartialEq for Owner only based on name
+impl std::cmp::PartialEq for Owner {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
+
+
+
 struct Car {
     model: String,
     year: u32,
@@ -35,12 +54,13 @@ pub fn get_car_details(){
     };
 
     car_map.insert(owner1, car1);
+    car_map.insert(owner2, car2);
 
-    // get car details for owner1
+    // get car details for owner with name John
 
     let owner_key = Owner {
         name: String::from("John"),
-        age: 30,
+        age: 0
     };
 
     match car_map.get(&owner_key) {
